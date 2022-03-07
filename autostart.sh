48f69e0ab3257060c3ccf279adf08ff1d3e38486
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2022-03-07 19:39:02 +0800

    Update

--- autostart.sh
+++ autostart.sh
@@ -7,6 +7,17 @@ function run {
     fi
 }
 
+# Systray
+run "nm-applet"
+run "volumeicon"
+run "pamac-tray"
+run "xfce4-power-manager"
+
+# System
 run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-run "nitrogen --restore"
+dwmstatus &
+
+# Theme
+feh --bg-fill -z ~/Pictures/Wallpapers/arcolinux/* &
+picom -b  --config ~/.config/arco-dwm/picom.conf &
